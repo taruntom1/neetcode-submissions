77343bd156f6be1341a3a1f2e521c6888327e340
Tarun Tom <64594622+taruntom1@users.noreply.github.com>
2026-08-09 20:16:57 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+   public:
+    int maxProfit(vector<int>& prices) {
+        auto it_b = prices.begin();
+        auto it_s = prices.begin();
+        auto it = prices.begin();
+        int max_profit = -10000000;
+
+        while (it != prices.end()) {
+            int profit = *it - *it_b;
+
+            auto it2 = it_s;
+            while (it2 != it) {
+                if (*it2 < *it_b) it_b = it2;
+                it2++;
+            }
+
+            it_s = it;
+            cout << *it_b << " " << *it_s << endl;
+
+            max_profit = (max_profit > (*it_s - *it_b)) ? max_profit : *it_s - *it_b;
+
+            it++;
+        }
+
+        return max_profit;
+    }
+};
