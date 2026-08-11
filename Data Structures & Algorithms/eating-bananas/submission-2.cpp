36e8e1da0723c8fd883e8950e03a8c9a54639d76
Tarun Tom <64594622+taruntom1@users.noreply.github.com>
2026-08-11 15:19:28 +0530

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+   public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int upper = *max_element(piles.begin(), piles.end());
+        int lower = 1;
+
+        while (upper >= lower) {
+            int r = lower + (upper - lower) / 2;
+            int f_time = finishTime(piles, r);
+            if (f_time > h)
+                lower = r + 1;
+            else
+                upper = r - 1;
+        }
+        return lower;
+    }
+
+    int finishTime(vector<int>& piles, int r) {
+        int time = 0;
+        for (int i : piles) {
+            time += ceil(static_cast<double>(i) / r);
+        }
+        return time;
+    }
+};
